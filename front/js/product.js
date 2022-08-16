e8a9da300a022deddd2c42ca43e48cbaca1eba55
ElodieCara <102527658+ElodieCara@users.noreply.github.com>
2022-08-16 10:11:44 +0200

    page produit fonctionnelle

---
+++ front/js/product.js
@@ -0,0 +1,166 @@
+// BESOIN PRINCIPAL : AFFICHER DONNEES DE CHAQUE CANAPE
+// Etape1: RécupéreR L'ID dans l'URL (location.href)
+// Etape2: remplacer l'id de l'élément par son identifiant
+// Etape3: récupérer dans la base de données 
+
+/* Rediriger l'URL dans l'API */
+function getProductId() {
+  return new URL(location.href).searchParams.get("id")
+  //location.href permet d'obtenir la chaine complète product, searchParams.get("id") on récupère l'Id de chaque article
+}
+
+/* Aller chercher l'Url */
+function getProduct(productId) {
+  return fetch(`http://localhost:3000/api/products/${productId}`) // on ajoute l'Id
+    .then(response => response.json())
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+
+
+function createImage(imageURL, altText) {
+  //On recréé les balises img et altText dans la div.item__img
+  const image = document.createElement("img")
+  image.src = imageURL
+  image.alt = altText
+  const parent = document.querySelector(".item__img")
+  if (parent != null) parent.appendChild(image)
+}
+
+
+
+function hydrateProduct(product) {
+  //On récupére nos informations
+  document.getElementById("title").textContent = product.name
+  document.getElementById("price").textContent = product.price
+  document.getElementById("description").textContent = product.description
+  document.getElementById('colors').insertAdjacentHTML("beforeend", product.colors.map(color => `<option value="${color}">${color}</option>`))
+  createImage(product.imageUrl, product.altTxt);
+}
+
+async function displayProduct() {
+  //récupére l'ID dans notre URL
+  const productId = getProductId()
+  //récupère un article
+  const product = await getProduct(productId)
+  hydrateProduct(product)
+}
+
+// Ajouter des produits dans la page panier
+function formatProduct(productId, color, quantity) {
+  return {
+    _id: productId,
+    color: color,
+    quantity: quantity,
+  }
+}
+
+/* LocalSotrage */
+function manageLocalStorage() {
+  document.getElementById("addToCart").addEventListener("click", e => {
+    e.preventDefault()
+    const productId = getProductId()
+    const color = document.getElementById('colors').value
+    const quantity = document.getElementById('quantity').valueAsNumber
+    const product = formatProduct(productId, color, quantity)
+    //Récupère des données du localStorage
+    let productsLocalStorage = JSON.parse(localStorage.getItem("product"))
+    //Si le localStorage existe
+    if (!productsLocalStorage) productsLocalStorage = []
+    //On recherche si l'Id d'un produit de même couleur est déjà dans le panier avec la méthode findIndex
+    const productIndex = productsLocalStorage.findIndex(p => p._id === productId && p.color === color)
+    console.log(productIndex)
+    // Si il n'y est pas, on push le nouvel article
+    if (productIndex === -1) {
+      productsLocalStorage.push(product)
+    }
+    //Sinon le nouvel article s'additionne
+    else {
+      productsLocalStorage[productIndex].quantity += quantity
+    }
+    localStorage.setItem("product", JSON.stringify(productsLocalStorage))
+  })
+}
+
+displayProduct();
+manageLocalStorage();
+
+//---------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+// const str = window.location.href;
+// const url = new URL(str);
+// const id = url.searchParams.get("id");
+// console.log(id);
+// var search_params = new URLSearchParams(url.search);
+// /*has() utilisé pour vérifier si le paramètre existe dans l'URL
+//   renvoie true si le paramètre existe, sinon false.
+//   Si existe, sa valeur peut être trouvée via la méthode get.*/
+// if (search_params.has('id')) {
+//     var id = search_params.get('id');
+//     console.log('id');
+// }
+
+
+////ou////
+
+// var chemin = window.location.pathname; //chemin reçoit le chemin de l'url
+// var url = window.location.href; //la variable reçoit l'url courante
+// créer une nouvelle url avec searchParams pour savoir quels produits afficher
+// let params = new URL(window.location.href).searchParams;
+
+////ou////
+
+// let searchParams = new URLSearchParams(window.location.search);
+//l'URL new + ID
+// let newID = params.get('id');
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+////Appeler new API avec ID du canapé séléctionné////
+
+// fetch()
+//   .then(function(res) {
+//   if (res.ok) {
+//     return res.json();
+//   }
+// })
+//     .then(function (value) {
+//       console.log(value);
+//     })
+//     .catch(function (err) {
+//       // Une erreur est survenue
+//     });
+
+// function getProduct() {
+//   return fetch(id)
+//     .then(response => response.json())
+//     .catch(error => {
+//       console.log(error);
+//     })
+// }
+
+
+// variables  Get /{product-ID} (doit être remplacé par l'ID d'un produit)
+// let colors = document.getElementById('colors');
+// // let id = document.getElementsByClassName('_id');
+// let name = document.getElementById('name');
+// let price = document.getElementById('price');
+// let imageURL = document.getElementsByClassName('item__img');
+// let description = document.getElementById('description');
+// // let altTxt = document.getElementsByClassName('altText');
+
+
+
+/////////////////////////////////////////////////////////////////////////
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,49 +1,34 @@
-// BESOIN PRINCIPAL : AFFICHER DES CANAPES
-// Etape1: Récupérer les canapés (appel au back)
-// Etape2: Formater la data de chaque canapé
-// Etape3: Afficher la liste de canapés
-
-// NORMALISATION
-// IMPORTANT --> INDENTATION
-// le code doit etre en ANGLAIS (FRANGLAIS accepté) 
-// chaque fonction ou variable sera écrit en camelCase: exemple const myVariable, const nowImADevelopper
-// le nom des fonctions doit etre significatif (exemple: function sum(),  function compareDistance())
-// le nom d'une fonction est commence par un verbe d'action (mauvais exemple: function distance())
-// le nom d'une variable doit etre significatif (mauvais exemple: const myVar, exemple: const distanceBetweenAandB, const firstName)
-// PAS DE CODE HORS DES FONCTIONS !!!!!!!!!!!!!
-// chiffre => Number
-// lettre => String
-// booléen => Boolean (false ou true)
-// objet => Object {} => const object = { firstName: 'Elodie', lastName: 'Cara' } 
-// object.firstName ==> Elodie
-// tableau => Array [] ([String], [Boolean], ...)
-// un tableau est une liste d'éléments => const array = ['Elodie', 'Cara'];
-// array[0] ==> Elodie / array[1] ==> Cara / array[2] ==> undefined / array.length = 2
-// si le nom de ma variable se termine par un s ==> c'est un tableau
+//Requête de l'API pour lui demander l'ensemble des produits
 
 function getProducts() {
     return fetch('http://localhost:3000/api/products')
         .then(response => response.json())
+        // .then(products => {
+        //     formatProducts(products);
+        // })
         .catch(error => {
             console.log(error);
-        });
+        });   
 }
 
+// Affichage catalogue produits
+
+
 function formatProducts(productsToFormat) {
-    // <a href="./product.html?id=42">
-    //     <article>
-    //         <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
-    //         <h3 class="productName">Kanap name1</h3>
-    //         <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-    //     </article>
-    // </a>
-    // Utiliser une boucle For (a voir sur internet)
-    // document.querySelector('#items').innerHtml = ...
+    for (let product of productsToFormat) {
+        document.querySelector('#items').innerHTML += `<a href="./product.html?id=${product._id}">
+            <article>
+                <img src="${product.imageUrl}" alt="${product.altTxt}">
+                    <h3 class="productName">${product.name}</h3>
+                    <p class="productDescription">${product.description}</p>
+            </article>
+        </a>`;
+    }
 }
 
 async function displayProducts() {
     const products = await getProducts();
-    if(products && products.length) {
+    if (products && products.length) {
         formatProducts(products);
     }
 }
